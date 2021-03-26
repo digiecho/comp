5347d37b282ac2b73c2b20daa298759916a0c2b2
ste <foalestephen@gmail.com>
2021-03-26 16:24:07 +0000

    Updates

---
+++ precache-manifest.e5d3852ec86c7c6dcb3c6c07ce09e712.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20b328f081e286e30765560ef0874bc3",
+    "url": "/comp/index.html"
+  },
+  {
+    "revision": "a13edeb2970372020a4e",
+    "url": "/comp/static/css/main.ccc4fc44.chunk.css"
+  },
+  {
+    "revision": "af0ca9ea13ebccdaee26",
+    "url": "/comp/static/js/2.99e1e09a.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/comp/static/js/2.99e1e09a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a13edeb2970372020a4e",
+    "url": "/comp/static/js/main.6d047f63.chunk.js"
+  },
+  {
+    "revision": "a87b326d5de9a1d860b2",
+    "url": "/comp/static/js/runtime-main.b96e1a78.js"
+  },
+  {
+    "revision": "40bdbb04ba29b14b770dc9caeb4917ca",
+    "url": "/comp/static/media/smallr.40bdbb04.gif"
+  }
+]);
